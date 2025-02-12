90648725f504fb73bc2aaae9239ad7b6d70d502e
takumakoike <takuma.151e@gmail.com>
2025-02-12 23:56:31 +0900

    #03 型の定義と関数の定義

---
+++ app/timer/page.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect, useDebugValue } from "react";
+
+type typeProjects = {
+    id: string;
+    name: string;
+    startTime: number | null;
+    stoppedTime: number | null;
+    restartTime: number | null;
+    endTime: number | null;
+    isRunning: boolean;
+}
+
+export default function Timer(){
+    //プロジェクトに関する状態管理
+    const [projects, setProjects] = useState<typeProjects[]>([]); 
+
+    // タイマー更新
+    useEffect(() => {
+
+    }, [])
+
+
+    // プロジェクトの追加
+    const addProject = () => {
+
+    }
+
+    const deleteProject = () => {}
+
+    const toggleTimer = () => {}
+
+    return (
+        <div>
+            <h1>稼働時間管理</h1>
+        </div>
+    )
+}
